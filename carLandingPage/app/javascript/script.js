0647096fb30cd7b99efcd795eae9a1ab915843fb
Mostafa Yahya mahmoud <as1229007@gmail.com>
2024-09-26 05:10:11 +0300

    Add files via upload
    edit in files

--- carLandingPage/app/javascript/script.js
+++ carLandingPage/app/javascript/script.js
@@ -17,3 +17,37 @@ const changeDisplay = () => {
   }
 };
 hamburger.addEventListener('click', changeDisplay);
+const carLeft = document.querySelector('.car-left');
+const carCenter = document.querySelector('.car-center');
+const carRight = document.querySelector('.car-right');
+const switchCar = ['./img/car1.png', './img/car2.png', './img/car3.png'];
+
+let x = 0;
+let y = 1;
+let z = 2;
+const createSrc = () => {
+  setInterval(() => {
+    carLeft.setAttribute('src', switchCar[x]);
+    x++;
+    if (x === 3) {
+      x = 0;
+    }
+  }, 1000);
+
+  setInterval(() => {
+    carCenter.setAttribute('src', switchCar[y]);
+    y++;
+    if (y === 3) {
+      y = 0;
+    }
+  }, 1000);
+
+  setInterval(() => {
+    carRight.setAttribute('src', switchCar[z]);
+    z++;
+    if (z === 3) {
+      z = 0;
+    }
+  }, 1000);
+};
+createSrc();
