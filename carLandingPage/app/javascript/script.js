9ed636af30b2169a56906d0f4267a68bd36ddcce
Mostafa Yahya mahmoud <as1229007@gmail.com>
2024-10-01 00:13:31 +0300

    Add files via upload
    011532548986568

--- carLandingPage/app/javascript/script.js
+++ carLandingPage/app/javascript/script.js
@@ -40,3 +40,11 @@ span.forEach((ele) => {
 });
 
 hamburger.addEventListener('click', changeHamburger);
+
+const box = document.querySelectorAll('.box');
+
+// box.forEach((ele) => {
+//   setInterval(() => {
+//     ele.classList.toggle('translate');
+//   }, 3000);
+// });
