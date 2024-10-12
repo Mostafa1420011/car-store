e810f5d11b2d30149e0f73c90166f85fadf6d385
Mostafa Yahya mahmoud <as1229007@gmail.com>
2024-10-13 02:50:08 +0300

    Add files via upload
    jhvhj

--- carLandingPage/app/javascript/script.js
+++ carLandingPage/app/javascript/script.js
@@ -1,18 +1,18 @@
-const img = document.querySelector('.img-change');
-const images = ['./img/car1.png', './img/car2.png', './img/car3.png'];
-let count = 0;
+// const img = document.querySelector('.img-change');
+// const images = ['./img/car1.png', './img/car2.png', './img/car3.png'];
+// let count = 0;
 
-const func = () => {
-  setInterval(() => {
-    img.setAttribute('src', images[count]);
-    count++;
-    if (count === 3) {
-      count = 0;
-    }
-  }, 2000);
-};
+// const func = () => {
+//   setInterval(() => {
+//     img.setAttribute('src', images[count]);
+//     count++;
+//     if (count === 3) {
+//       count = 0;
+//     }
+//   }, 2000);
+// };
 
-func();
+// func();
 const span = document.querySelectorAll('[data-name]');
 const hamburger = document.querySelector('.hamburger');
 const ul = document.querySelector('.ul');
